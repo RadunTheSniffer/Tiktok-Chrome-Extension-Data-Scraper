8aecb90b4f05bf4c97fb6efb5fd53422bb6ce73d
RadunTheSniffer <ridhuan.work2001@gmail.com>
2024-09-19 08:58:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Output Page');
 });
 
 app.post('/scrape-tiktok', async (req, res) => {
